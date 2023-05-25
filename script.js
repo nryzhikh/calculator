b5c0a572fa88538ded3dc577c51bd6a2a29db5b8
nryzhikh <nikita.ryzhikh@outlook.com>
2023-05-25 21:00:07 +0400

    Add . and backspace buttons and functions for them Add Error message if divide by 0 Add keyboard support

--- script.js
+++ script.js
@@ -2,37 +2,37 @@ const display = document.querySelector('#display');
 const wrapper = document.getElementById('grid');
 let number1 = null;
 let number2 = '';
-const operators = document.querySelectorAll('button.operator');
-const operatorsArray = Array.from(operators, el => el.textContent);
+const buttons = document.querySelectorAll('button');
+const buttonsArray = Array.from(buttons, el => el.textContent);
 
-const add = function(number1, number2) {
-    return number1 + number2;
+function add(a, b) {
+    return a + b;
 };
 
-const subtract =function(number1, number2) {
-    return number1 - number2
+function subtract(a, b) {
+    return a - b;
 };
 
-const multiply = function (number1, number2) {
-    return number1 * number2
+function multiply(a, b) {
+    return a * b;
 };
 
-const divide = function(number1, number2) {
-    return number1 / number2
+function divide(a, b) {
+    return a / b
 };
 
 const operate = function(number1, number2, operator) {
     if (operator == "+") {
-        return add(number1, number2)
+        return add(parseFloat(number1), parseFloat(number2))
     }
     else if (operator == "-") {
-        return subtract(number1, number2)
+        return subtract(parseFloat(number1), parseFloat(number2))
     }
     else if (operator == "*") {
-        return multiply(number1, number2)
+        return multiply(parseFloat(number1), parseFloat(number2))
     } 
     else if (operator == "/") {
-        return divide(number1, number2)
+        return divide(parseFloat(number1), parseFloat(number2))
     } else if (operator == "=") {
         return number2
     }
@@ -45,11 +45,52 @@ resetFlag = true;
 
 function evaluate () {
     if (operator === null || resetFlag) return
-    number2 = parseInt(display.value);
+    if (operator == "/" && number2 == 0) {
+        display.value = "Error";
+        number1 = '';
+        number2 = '';
+        operator = '=';
+    } else {
     number1 = operate(number1, number2, operator);
     display.value = number1;
     number2 = ""; 
+    }
 };
+
+function addFloat () {
+    if (display.value.includes(".") ) return
+    display.value += ".";
+    number2 = number2 + ".";
+};
+
+window.addEventListener('keydown', function(event){
+    if (['+','-','*','/','=' ].indexOf(event.key) != -1) {
+        evaluate();
+        operator = event.key;
+        resetFlag = true;
+    } else if (event.key == "C") {
+        number1 = '';
+        number2 = '';
+        operator = '=';
+        display.value = 0;
+    } else if (event.key >= 0 && event.key <= 9) {
+        number2 += event.key;
+        display.value = number2;
+        resetFlag = false;
+    } else if (event.key === ".") {
+        addFloat();
+    } else if (event.key === "Backspace") {
+        display.value = display.value.slice(0, -1);
+        number2 = display.value;
+    } else if (event.key === "Enter") {
+        event.preventDefault();
+        evaluate();
+        operator = "=";
+        resetFlag = true;
+    };
+    console.log(event);
+    console.log(number2);
+});
 
 wrapper.addEventListener('click', (event) => {
     let buttonValue = event.target.textContent;
@@ -57,18 +98,22 @@ wrapper.addEventListener('click', (event) => {
         evaluate ();
         operator = buttonValue;
         resetFlag = true;
-
     } else if (buttonValue == "C") {
         number1 = null;
         number2 = '';
         operator = '=';
         display.value = 0;
-    } 
-    else if (buttonValue >= 0 && buttonValue <= 9) {
-        number2 += event.target.textContent
-        display.value = number2;
+    } else if (buttonValue >= 0 && buttonValue <= 9) {
+        number2 += event.target.textContent;
+        display.value = parseFloat(number2);
         resetFlag = false;
+    } else if (buttonValue === ".") {
+        addFloat();
+    } else if (buttonValue === "back") {
+        display.value = display.value.slice(0, -1);
+        number2 = display.value;
     };
     console.log(number1);
     console.log(number2);
 });
+
